9a87d452a766e07922dfb2d1d16d73d83b901403
erdi_gurbuz <erdi.gurbuz@ttgint.com>
2015-11-13 21:05:27 +0200

    haberbüs parser ve handler'lar eklendi

---
+++ LogHandler.py
@@ -0,0 +1,33 @@
+import sys
+import traceback
+from datetime import datetime
+
+class LogHandler:
+    def __init__(self, className):
+        self.className = className
+    
+    def printMsg(self, message, tabStop=0):
+        if not tabStop:
+            print "\t" * tabStop + "* %s"%message
+        elif tabStop == 1:
+            print "\t" * tabStop + "# %s"%message
+        elif tabStop == 2:
+            print "\t" * tabStop + "- %s"%message
+        elif tabStop == 3:
+            print "\t" * tabStop + ". %s"%message
+
+        sys.stdout.flush()
+
+    # Hata ve uyarilarin loglanmasi
+    def logger(self, function, message=""):
+        #Dosyaya
+        with open("error.log", "a") as f:
+            f.writelines("[%s]\n%s.%s %s\n"%(str(datetime.now())[:19], self.className, function, message))
+            traceback.print_exc(file=f)
+            f.writelines("\n")
+        #Ekrana
+        print "[%s]\n%s.%s %s\n"%(str(datetime.now())[:19], self.className, function, message),
+        print traceback.format_exc()
+        print "\n"
+        sys.stdout.flush()
+
---
+++ ServerDatabaseHandler.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import MySQLdb
+from LogHandler import LogHandler
+
+class ServerDatabaseHandler:
+	def __init__(self):
+		try:
+			self.logHandler = LogHandler("ServerDatabaseHandle")
+			
+			self.serverHandler = MySQLdb.connect('127.0.0.1', 'root', 'yourdbpassowrd', 'haberbus');
+			self.serverHandler.set_character_set('utf8')
+			self.serverHandler.autocommit(True)
+			
+			self.cursor = self.serverHandler.cursor()
+			self.cursor.execute('SET NAMES utf8;')
+			self.cursor.execute('SET CHARACTER SET utf8;')
+			self.cursor.execute('SET character_set_connection=utf8;')
+			
+		except:
+			self.logHandler.logger("__init__")
+	
+	def executeQuery(self, query):
+		self.cursor.execute(query)
+		if query.split()[0].lower() == "select":
+			return self.cursor.fetchall()
+		else:
+			self.serverHandler.commit()
+		
+	def closeConnection(self):
+		self.serverHandler.close()
