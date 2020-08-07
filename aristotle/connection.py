3a5c08fee183cc8af59bf9b3cf850baa9aeba8b2
egcodes <erdigurbuz@gmail.com>
2020-08-07 23:42:21 +0300

    Add singleton pattern for DB conn

--- aristotle/connection.py
+++ aristotle/connection.py
@@ -3,14 +3,24 @@ from settings import *
 
 
 class Database:
-    def __init__(self):
-        props = getProps("database")
-        self.engine = db.create_engine("mysql+pymysql://%s:%s@%s:%s/%s"
-                                       % (props["userName"], props["password"],
-                                          props["url"], props["port"], props["name"]))
+    __instance__ = None
 
-        self.connection = self.engine.connect()
-        self.metadata = db.MetaData()
+    def __init__(self):
+        if Database.__instance__ is None:
+            Database.__instance__ = self
+            dbProps = getProps("database")
+            self.engine = db.create_engine("mysql+pymysql://%s:%s@%s:%s/%s"
+                                           % (dbProps["userName"], dbProps["password"],
+                                              dbProps["url"], dbProps["port"], dbProps["name"]))
+
+            self.connection = self.engine.connect()
+            self.metadata = db.MetaData()
+
+    @staticmethod
+    def get_instance():
+        if not Database.__instance__:
+            Database()
+        return Database.__instance__
 
     def getDB(self):
         return db
@@ -24,8 +34,9 @@ class Database:
     def getConnection(self):
         return self.connection
 
-    def getMeta(self, tableName):
-        return db.Table(tableName, self.metadata, autoload=True, autoload_with=self.engine)
+    def getMeta(self):
+        return self.metadata
+        # return db.Table(tableName, self.metadata, autoload=True, autoload_with=self.engine)
 
     def closeDB(self):
         self.connection.invalidate()
